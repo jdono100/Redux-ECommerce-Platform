74ba26f44d2d53d32b7f53318e4815f03b396e3e
Jared Donovan <jdono100@gmail.com>
2021-09-22 22:54:27 -0500

    fix store.js export

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,4 +1,6 @@
 import { createStore } from '@reduxjs/toolkit';
 import reducer from '../utils/reducers';
 
-export const store = createStore(reducer);
+const store = createStore(reducer);
+
+export default store;
